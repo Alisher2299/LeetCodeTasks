b4220154e72388c0d79197ca4fff070130d9fc06
Alisher Zhanayev <zhanayev.a@chocofood.kz>
2022-04-12 17:33:42 +0600

    add array

--- Dynamic Programming /Climbing Stairs.py
+++ Dynamic Programming /Climbing Stairs.py
@@ -5,18 +5,28 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 """
 
 
+# def climbStairs(n: int) -> int:
+#     if n <= 2:
+#         return n
+#
+#     step1 = 1
+#     step2 = 2
+#
+#     for i in range(3, n + 1):
+#         sum = step1 + step2
+#         step1 = step2
+#         step2 = sum
+#     return step2
+
+
 def climbStairs(n: int) -> int:
-    if n <= 2:
-        return n
-
-    step1 = 1
-    step2 = 2
-
-    for i in range(3, n + 1):
-        sum = step1 + step2
-        step1 = step2
-        step2 = sum
-    return step2
+    """
+    best decision
+    """
+    a = b = 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
 
 print(climbStairs(5))
